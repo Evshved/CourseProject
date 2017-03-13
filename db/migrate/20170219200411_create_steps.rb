6a86a06bb0f89be303585be7558ad8605efd36f0
Shved <snuggleshved@gmail.com>
2017-03-13 15:23:30 +0300

    Updates.

--- db/migrate/20170219200411_create_steps.rb
+++ db/migrate/20170219200411_create_steps.rb
@@ -2,7 +2,7 @@ class CreateSteps < ActiveRecord::Migration[5.0]
   def change
     create_table :steps do |t|
       t.string 'img'
-      t.string 'description'
+      t.text 'description'
       t.integer 'order'
       t.integer 'instruction_id', null: false
       t.timestamps
